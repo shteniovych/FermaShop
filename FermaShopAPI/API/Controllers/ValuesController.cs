b90f2848116280fbfcc71dbfe333fa2cac792dcf
Viktor Shtenovych <victor.shteniovych@gmail.com>
2019-01-04 11:42:32 +0200

    changed api method

--- FermaShopAPI/API/Controllers/ValuesController.cs
+++ FermaShopAPI/API/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "value";
+            return "valueeeeeeeeee";
         }
 
         // POST api/values
